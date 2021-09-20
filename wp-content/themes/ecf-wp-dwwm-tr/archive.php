1fa473c0468e32733ad9f85ecd54fc8b6bab199c
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-21 00:04:04 +0200

    fix: customize post code for news

--- wp-content/themes/ecf-wp-dwwm-tr/archive.php
+++ wp-content/themes/ecf-wp-dwwm-tr/archive.php
@@ -5,15 +5,14 @@
 <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
 	<article class="post">
-		<h2><?php the_title(); ?></h2>
-
+		
 		<?php the_post_thumbnail(); ?>
 		
 		<p class="post__meta">
-			Publié le <?php the_time( get_option( 'date_format' ) ); ?> 
-			par <?php the_author(); ?> • <?php comments_number(); ?>
+			<?php the_time( 'd/m/Y' ); ?>
 		</p>
-		
+		<h2><?php the_title(); ?></h2>
+
 		<?php the_excerpt(); ?>
 		
 		<p>
