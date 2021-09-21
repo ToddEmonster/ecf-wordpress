6d3bc49e61c7a5ce2123e57ab2a8049bf30340fa
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-21 23:40:44 +0200

    feat: refacto functions and add scripts

---
+++ wp-content/themes/ecf-wp-dwwm-tr/inc/features.php
@@ -0,0 +1,30 @@
+<?php
+
+function todd_register_assets() {
+
+    // Déclarer le JS
+	wp_enqueue_script( 
+        'nav', 
+        get_template_directory_uri() . '/js/nav.js', 
+        array(), 
+        '1.0', 
+        true
+    );
+
+    // Déclarer style.css à la racine du thème
+    wp_enqueue_style( 
+        'style',
+        get_stylesheet_uri(), 
+        array(), 
+        '1.0'
+    );
+
+    // TODO Déclarer un autre fichier CSS
+    wp_enqueue_style( 
+        'style_bis', 
+        get_template_directory_uri() . '/css/main.css',
+        array(), 
+        '1.0'
+    );
+}
+add_action( 'wp_enqueue_scripts', 'register_assets' );
