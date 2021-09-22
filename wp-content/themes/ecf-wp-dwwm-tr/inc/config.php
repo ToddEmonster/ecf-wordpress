ab0d2967aad1faf193c81bab72975708020dd063
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-22 20:10:27 +0200

    feat: set post thumbnail size

--- wp-content/themes/ecf-wp-dwwm-tr/inc/config.php
+++ wp-content/themes/ecf-wp-dwwm-tr/inc/config.php
@@ -9,4 +9,25 @@ add_theme_support( 'title-tag' );
 function todd_remove_menu_pages() {
     remove_menu_page( 'edit-comments.php' );
 }
-add_action( 'admin_menu', 'remove_menu_pages' );
+add_action( 'admin_menu', 'todd_remove_menu_pages' );
+
+// TODO Définir la taille des images mises en avant
+set_post_thumbnail_size( 400, 260, true );
+
+// TODO
+// /* Création de tailles d'images personnalisées */
+// add_image_size('archive_thumbnail', 540, 500, true);
+// add_image_size( 'portfolio_featured', 1100, 500, true );
+// add_image_size( 'portfolio_preview', 635, 400, true );
+
+// /* Ajout des tailles d'images dans l'éditeur */
+
+// function todd_editor_image_sizes( $sizes ) {
+//     $sizes = array_merge( $sizes, array(
+//         'archive_thumbnail' => 'Archive 540 x 500',
+//         'portfolio_featured' => 'Portfolio featured 1100 x 500',
+//         'portfolio_preview' => 'Portfolio preview 635 x 400',
+//     ));
+//     return $sizes;
+// }
+// add_filter( 'image_size_names_choose', 'todd_editor_image_sizes' );
