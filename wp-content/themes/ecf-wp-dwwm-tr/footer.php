802ebb78af30cd13a8944573e1991c52f36c0cba
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-22 20:59:25 +0200

    feat: add footer

--- wp-content/themes/ecf-wp-dwwm-tr/footer.php
+++ wp-content/themes/ecf-wp-dwwm-tr/footer.php
@@ -1,5 +1,16 @@
-    <footer class="site__footer">
-		<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+    <footer class="main-footer">
+		<div class="container">
+			<address>
+				<strong>Développeur Web et Web Mobile</strong><br>
+				CEFIM<br>
+				32 Avenue Marcel Dassault<br>
+				37200 Tours<br>
+				T : <a href="tel:0247402680">02 47 40 26 80</a>
+			</address>
+			<?php wp_nav_menu( array(
+				'theme_location' => 'footer',
+				'menu_class' => 'footer-nav menu' ) ); ?>
+		</div>
 	</footer>
 
 	<?php wp_footer(); ?>
--- wp-content/themes/ecf-wp-dwwm-tr/header.php
+++ wp-content/themes/ecf-wp-dwwm-tr/header.php
@@ -8,6 +8,8 @@
 </head>
 
 <body <?php body_class(); ?>>
+    <a href="#main-menu" class="screen-reader-text">Aller à la navigation principale</a>
+	<a href="#main-content" class="screen-reader-text">Aller au contenu principal</a>
     <header class="main-header">
         <div class="container">
             <div class="logo"><a href="<?php echo home_url( '/' ); ?>">DWWM</a></div>
@@ -16,7 +18,6 @@
                     'theme_location' => 'main',
                     'menu_class' => 'main-nav menu', // ma classe personnalisée 
             )  ); ?>
-
         </div>
     </header>
     <?php wp_body_open(); ?>
