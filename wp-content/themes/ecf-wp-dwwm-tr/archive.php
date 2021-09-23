10c6a727886e7b9c1741f59755b470006d4f70d3
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-23 23:03:49 +0200

    feat: add pagination for archive template

--- wp-content/themes/ecf-wp-dwwm-tr/archive.php
+++ wp-content/themes/ecf-wp-dwwm-tr/archive.php
@@ -1,11 +1,22 @@
 <?php get_header(); ?>
 
-<h1>ARCHIVE</h1>
+<main id="main-content" class="archive">
+	<div class="container">
 
-<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+		<h1 class="section-title">ARCHIVE</h1>
 
-	<?php get_template_part( 'parts/archive-item' ); ?>
+		<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'parts/archive-item' ); ?>
+		<?php endwhile; endif; ?>
 
-<?php endwhile; endif; ?>
+		<?php the_posts_pagination( array(
+			'class' => 'pagination',
+			'aria_label' => 'Archive items',
+			'prev_text' => '«',
+			'next_text' => '»',
+		)); ?>
+
+	</div>
+</main>
 
 <?php get_footer(); ?>
