6e4aee479f5584e829a9d8161cf0742edac8035f
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-23 23:02:36 +0200

    feat: add pagination for News

--- wp-content/themes/ecf-wp-dwwm-tr/actualites.php
+++ wp-content/themes/ecf-wp-dwwm-tr/actualites.php
@@ -2,10 +2,20 @@
 
 <main id="main-content" class="last-news">
 	<div class="container">
+
 		<h1 class="section-title">ACTUALITÉS</h1>
+
 		<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'parts/archive-item' ); ?>
 		<?php endwhile; endif; ?>
+
+		<?php the_posts_pagination( array(
+			'class' => 'pagination',
+			'aria_label' => 'Actualités',
+			'prev_text' => '«',
+			'next_text' => '»',
+		)); ?>
+
 	</div>
 </main>
 
