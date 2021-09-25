c3bf5781ea2bc25b4f652fab8047bda668354d86
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-25 18:39:12 +0200

    feat: add all plugins

--- wp-config.php
+++ wp-config.php
@@ -22,6 +22,8 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', '/Applications/MAMP/htdocs/ecf-wordpress/wp-content/plugins/wp-super-cache/' );
 define( 'DB_NAME', 'ecf-wordpress' );
 
 /** Utilisateur de la base de données MySQL. */
