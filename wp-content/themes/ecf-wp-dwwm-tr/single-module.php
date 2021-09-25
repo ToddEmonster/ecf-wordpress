fea68b2c9f8f1ed4edccc40ec84bef1e1095702b
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-09-26 01:05:27 +0200

    feat: finish module single

--- wp-content/themes/ecf-wp-dwwm-tr/single-module.php
+++ wp-content/themes/ecf-wp-dwwm-tr/single-module.php
@@ -1,26 +1,20 @@
 <?php get_header(); ?>
 
-<main id="main-content" class="student-post">
-    <section class="module-hero">
+<main id="main-content" class="post">
+    <section
+        class="module-hero"
+        style="background-image: url(
+            <?php the_post_thumbnail_url('single_module') ?>
+        );">
         <div class="container">
-            <h1 class="student-post-title">Module <?php the_title(); ?></h1>
+            <h1>Module <?php the_title(); ?></h1>
         </div>
     </section>
     <section class="module-desc">
         <div class="container container-narrow">
-            <p><?php the_content(); ?></p>
+            <?php the_content(); ?>
         </div>
     </section>
-        <?php
-            /* $image_id = get_field( 'photo' );
-                if( $image_id ) {	
-                    echo wp_get_attachment_image(
-                        $image_id,
-                        'full',
-                        '',
-                        array( "class" => "student-post-img" )
-                    ); 
-        } */ ?>
 </main>
 
 <?php get_footer(); ?>
